723cc3df4809461c56d18f6f8cc6071a21867eac
Ignacio <39157472+ignacioetchepare@users.noreply.github.com>
2018-05-17 20:41:59 -0300

    Update EliminarSotck.vb

--- proyecto.inicio/EliminarSotck.vb
+++ proyecto.inicio/EliminarSotck.vb
@@ -3,15 +3,20 @@
 Public Class EliminarSotck
 
     Private Sub DataGridView1_CellContentClick(sender As System.Object, e As System.Windows.Forms.DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
-        Dim sql As String
-        Dim conexion As New MySqlConnection
+       Dim conexion As New MySqlConnection
         Dim comando As MySqlCommand
+        Dim sql As String
 
         conexion = conn
         sql = "SELECT * FROM stock"
         conexion.Open()
-        comando = New MySqlCommand(sql, conexion)
 
+        comando = New MySqlCommand(sql, conexion)
+        Dim dt As New DataTable
+        Dim da As New MySqlDataAdapter(comando)
+        da.Fill(dt)
+        dgvStock.DataSource = dt
+        conexion.Clone()
 
 
 
