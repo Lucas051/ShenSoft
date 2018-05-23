819ac51b344ff6c8d55c128bc55fb998b87dbc95
Ignacio <39157472+ignacioetchepare@users.noreply.github.com>
2018-05-22 21:49:26 -0300

    solucionado

--- proyecto.inicio/ConsultaFactura.vb
+++ proyecto.inicio/ConsultaFactura.vb
@@ -1,22 +1,5 @@
 ﻿Imports MySql.Data.MySqlClient
 Public Class ConsultaFactura
-
-    Private Sub DataGridView1_CellContentClick(ByVal sender As System.Object, ByVal e As System.Windows.Forms.DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
-        Dim sql As String
-        Dim connect As New MySqlConnection
-        Dim comando As MySqlCommand
-        Try
-            connect = conn
-            sql = "SELECT * FROM facturas"
-            connect.Open()
-            comando = New MySqlCommand(sql, connect)
-            comando.ExecuteNonQuery()
-
-        Catch ex As Exception
-            MessageBox.Show(ex.ToString)
-        End Try
-    End Sub
-
     Private Sub Atras_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Atras.Click
         Me.Hide()
         Facturacion.Show()
@@ -24,6 +7,20 @@ Public Class ConsultaFactura
     End Sub
 
     Private Sub ConsultaFactura_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        Dim sql As String
+        Dim connect As New MySqlConnection
+        Dim comando As MySqlCommand
 
+        connect = conn
+        sql = "SELECT * FROM facturas"
+        connect.Open()
+        comando = New MySqlCommand(sql, connect)
+        comando.ExecuteNonQuery()
+
+        Dim dt As New DataTable
+        Dim da As New MySqlDataAdapter(comando)
+        da.Fill(dt)
+        DataGridView1.DataSource = dt
+        connect.Close()
     End Sub
 End Class
