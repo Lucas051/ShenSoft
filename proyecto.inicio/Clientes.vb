00b7b0226d271bb108a226ad1eef8b5eb98257f5
Facu12Ruiz <39156387+Facu12Ruiz@users.noreply.github.com>
2018-06-12 11:13:30 -0300

    Empeze clientes.
    .

---
+++ proyecto.inicio/Clientes.vb
@@ -0,0 +1,10 @@
+﻿Public Class Clientes
+
+    Private Sub Clientes_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        'aca voy a agregar algo
+    End Sub
+End Class
