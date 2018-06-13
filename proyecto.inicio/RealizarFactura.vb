78c6075aededa3a9b8157e892235c2ccab295b91
LucasFerreira <luc45f3rr3ir4@gmail.com>
2018-06-13 18:38:12 -0300

    Equisde

--- proyecto.inicio/RealizarFactura.vb
+++ proyecto.inicio/RealizarFactura.vb
@@ -1,6 +1,7 @@
 ﻿Imports MySql.Data.MySqlClient
 
 Public Class RealizarFactura
+    Dim cantidadp, valor, descuentop As Integer
 
     Private Sub DataGridView1_CellContentClick(ByVal sender As System.Object, ByVal e As System.Windows.Forms.DataGridViewCellEventArgs) Handles DataGridFacturas.CellContentClick
 
@@ -9,11 +10,6 @@ Public Class RealizarFactura
     Private Sub atras_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles atras.Click
         Me.Hide()
         Facturacion.Show()
-    End Sub
-
-    Private Sub RealizarFactura_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-
-
     End Sub
 
 
@@ -29,5 +25,19 @@ Public Class RealizarFactura
         cant.Text = ""
         descuent.Text = ""
         precioov.Text = ""
+    End Sub
+
+    Private Sub agregar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles agregar.Click
+        'No toquen esto, tiene un error we ahi lo soluciono, ahora voy a estudiar para filosofia y form empresarial xdxdxd lol
+        cantidadp = Val(cantidad.Text)
+        descuentop = Val(desc.Text)
+
+        valor = Val(precioov.Text) * Val(cantidadp)
+        descuentop = Val(precioov) * Val(desc) / 100
+        valor = (Val(precioov.Text) * Val(cantidadp)) - Val(descuentop)
+
+
+
+        DataGridFacturas.Rows.Add(codigo.Text, desc.Text, cantidadp, descuento, valor)
     End Sub
 End Class
