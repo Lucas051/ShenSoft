119d9d9ec0e3c30da286ae1e5a29969c73ca7128
LucasFerreira <luc45f3rr3ir4@gmail.com>
2018-06-14 21:22:05 -0300

    Al perecer anda

--- proyecto.inicio/Buscar proveedores.vb
+++ proyecto.inicio/Buscar proveedores.vb
@@ -5,6 +5,13 @@
     End Sub
 
     Private Sub Buscar_proveedores_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-
+        'comentariossssss
+        'comentariossssss
+        'comentariossssss
+        'comentariossssss
+        'comentariossssss
+        'comentariossssss'comentariossssss'comentariossssss'comentariossssss
+        'comentariossssss
+        'comentariossssss
     End Sub
 End Class
