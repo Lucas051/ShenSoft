16dab6120d5ffe8162b825c1e96e42a345e6251a
Ignacio <39157472+ignacioetchepare@users.noreply.github.com>
2018-06-14 22:44:22 -0300

    Actualizar el dgv version nacho

--- proyecto.inicio/Stock de Productos.Designer.vb
+++ proyecto.inicio/Stock de Productos.Designer.vb
@@ -34,13 +34,17 @@ Partial Class Stock_de_Productos
         Me.Button4 = New System.Windows.Forms.Button()
         Me.Button5 = New System.Windows.Forms.Button()
         Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.txtcantidad = New System.Windows.Forms.TextBox()
+        Me.txtproveedor = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(30, 41)
+        Me.Label1.Location = New System.Drawing.Point(30, 22)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(63, 13)
         Me.Label1.TabIndex = 0
@@ -49,7 +53,7 @@ Partial Class Stock_de_Productos
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(59, 66)
+        Me.Label2.Location = New System.Drawing.Point(59, 48)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(34, 13)
         Me.Label2.TabIndex = 1
@@ -58,7 +62,7 @@ Partial Class Stock_de_Productos
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(10, 93)
+        Me.Label3.Location = New System.Drawing.Point(10, 77)
         Me.Label3.Name = "Label3"
         Me.Label3.Size = New System.Drawing.Size(83, 13)
         Me.Label3.TabIndex = 2
@@ -67,7 +71,7 @@ Partial Class Stock_de_Productos
         'Label4
         '
         Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(-2, 9)
+        Me.Label4.Location = New System.Drawing.Point(30, 9)
         Me.Label4.MaximumSize = New System.Drawing.Size(300, 400)
         Me.Label4.Name = "Label4"
         Me.Label4.Size = New System.Drawing.Size(147, 13)
@@ -76,7 +80,7 @@ Partial Class Stock_de_Productos
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(112, 125)
+        Me.Button1.Location = New System.Drawing.Point(112, 152)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(75, 23)
         Me.Button1.TabIndex = 4
@@ -85,37 +89,37 @@ Partial Class Stock_de_Productos
         '
         'txtdescripcion
         '
-        Me.txtdescripcion.Location = New System.Drawing.Point(99, 38)
+        Me.txtdescripcion.Location = New System.Drawing.Point(99, 22)
         Me.txtdescripcion.Name = "txtdescripcion"
         Me.txtdescripcion.Size = New System.Drawing.Size(100, 20)
         Me.txtdescripcion.TabIndex = 6
         '
         'txtprecio
         '
-        Me.txtprecio.Location = New System.Drawing.Point(99, 63)
+        Me.txtprecio.Location = New System.Drawing.Point(99, 48)
         Me.txtprecio.Name = "txtprecio"
         Me.txtprecio.Size = New System.Drawing.Size(100, 20)
         Me.txtprecio.TabIndex = 7
         '
         'txtpreciostock
         '
-        Me.txtpreciostock.Location = New System.Drawing.Point(99, 90)
+        Me.txtpreciostock.Location = New System.Drawing.Point(99, 74)
         Me.txtpreciostock.Name = "txtpreciostock"
         Me.txtpreciostock.Size = New System.Drawing.Size(100, 20)
         Me.txtpreciostock.TabIndex = 8
         '
         'Button3
         '
-        Me.Button3.Location = New System.Drawing.Point(12, 154)
+        Me.Button3.Location = New System.Drawing.Point(1, 197)
         Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(92, 60)
+        Me.Button3.Size = New System.Drawing.Size(81, 31)
         Me.Button3.TabIndex = 9
         Me.Button3.Text = "Volver"
         Me.Button3.UseVisualStyleBackColor = True
         '
         'Button4
         '
-        Me.Button4.Location = New System.Drawing.Point(112, 154)
+        Me.Button4.Location = New System.Drawing.Point(112, 181)
         Me.Button4.Name = "Button4"
         Me.Button4.Size = New System.Drawing.Size(75, 22)
         Me.Button4.TabIndex = 10
@@ -124,9 +128,9 @@ Partial Class Stock_de_Productos
         '
         'Button5
         '
-        Me.Button5.Location = New System.Drawing.Point(112, 183)
+        Me.Button5.Location = New System.Drawing.Point(1, 150)
         Me.Button5.Name = "Button5"
-        Me.Button5.Size = New System.Drawing.Size(75, 45)
+        Me.Button5.Size = New System.Drawing.Size(75, 41)
         Me.Button5.TabIndex = 11
         Me.Button5.Text = "Modificar" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Tabla"
         Me.Button5.UseVisualStyleBackColor = True
@@ -140,11 +144,47 @@ Partial Class Stock_de_Productos
         Me.DataGridView1.Size = New System.Drawing.Size(551, 219)
         Me.DataGridView1.TabIndex = 12
         '
+        'txtcantidad
+        '
+        Me.txtcantidad.Location = New System.Drawing.Point(99, 100)
+        Me.txtcantidad.Name = "txtcantidad"
+        Me.txtcantidad.Size = New System.Drawing.Size(100, 20)
+        Me.txtcantidad.TabIndex = 13
+        '
+        'txtproveedor
+        '
+        Me.txtproveedor.Location = New System.Drawing.Point(99, 126)
+        Me.txtproveedor.Name = "txtproveedor"
+        Me.txtproveedor.Size = New System.Drawing.Size(100, 20)
+        Me.txtproveedor.TabIndex = 14
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(44, 100)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(49, 13)
+        Me.Label5.TabIndex = 15
+        Me.Label5.Text = "Cantidad"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(37, 126)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(56, 13)
+        Me.Label6.TabIndex = 16
+        Me.Label6.Text = "Proveedor"
+        '
         'Stock_de_Productos
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(758, 230)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.txtproveedor)
+        Me.Controls.Add(Me.txtcantidad)
         Me.Controls.Add(Me.DataGridView1)
         Me.Controls.Add(Me.Button5)
         Me.Controls.Add(Me.Button4)
@@ -176,4 +216,8 @@ Partial Class Stock_de_Productos
     Friend WithEvents Button4 As System.Windows.Forms.Button
     Friend WithEvents Button5 As System.Windows.Forms.Button
     Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+    Friend WithEvents txtcantidad As System.Windows.Forms.TextBox
+    Friend WithEvents txtproveedor As System.Windows.Forms.TextBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
 End Class
--- proyecto.inicio/Stock de Productos.vb
+++ proyecto.inicio/Stock de Productos.vb
@@ -12,13 +12,13 @@ Public Class Stock_de_Productos
 
     Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
 
-      
+
         Try
 
             If (txtdescripcion.Text <> "" And txtprecio.Text <> "" And txtpreciostock.Text <> "") Then
 
-                conexioon.Consulta = "INSERT INTO stock (descripcion,precio_costo,precio_v_stock) VALUES ('" + txtdescripcion.Text + "' , '" + txtprecio.Text + "','" + txtpreciostock.Text + "' )"
-                
+                conexioon.Consulta = "INSERT INTO stock (descripcion,cantidad,precio_costo,precio_v_stock) VALUES ('" + txtdescripcion.Text + "' , '" + txtcantidad.Text + "','" + txtprecio.Text + "','" + txtpreciostock.Text + "' )"
+
                 consultar()
                 DataGridView1.DataSource = resultado
 
@@ -33,13 +33,28 @@ Public Class Stock_de_Productos
         Catch ex As Exception
             MessageBox.Show(ex.ToString)
         End Try
-        
+        'Acaaaaaaaaaaa Actualizooooooooooooooo El DGV de mierdaaaaaaaaaaaaaaaaaaaa repitiendo el comando creo que se puede mejorar...
+        conexioon.Consulta = "SELECT * FROM stock"
+        consultar()
+        DataGridView1.DataSource = resultado
+
+        'Cambiar el nombre que tienen las columnas de Mysql esos "cod_producto" etc
+        DataGridView1.Columns(0).HeaderText = "Código"
+        DataGridView1.Columns(1).HeaderText = "Descripcion"
+        DataGridView1.Columns(2).HeaderText = "Cantidad"
+        DataGridView1.Columns(3).HeaderText = "Precio Costo"
+        DataGridView1.Columns(4).HeaderText = "Precio Venta"
+        DataGridView1.Columns(5).HeaderText = "Proveedor"
+        'Proximamente Solo en Cines
+
     End Sub
 
     Private Sub Button4_Click(sender As System.Object, e As System.EventArgs) Handles Button4.Click
 
         txtdescripcion.Text = ""
         txtprecio.Text = ""
+        txtcantidad.Text = ""
+        txtproveedor.Text = ""
         txtpreciostock.Text = ""
 
 
@@ -60,9 +75,10 @@ Public Class Stock_de_Productos
         'Cambiar el nombre que tienen las columnas de Mysql esos "cod_producto" etc
         DataGridView1.Columns(0).HeaderText = "Código"
         DataGridView1.Columns(1).HeaderText = "Descripcion"
-        DataGridView1.Columns(2).HeaderText = "Precio Costo"
-        DataGridView1.Columns(3).HeaderText = "Precio Venta"
-        DataGridView1.Columns(4).HeaderText = "Proveedor"
+        DataGridView1.Columns(2).HeaderText = "Cantidad"
+        DataGridView1.Columns(3).HeaderText = "Precio Costo"
+        DataGridView1.Columns(4).HeaderText = "Precio Venta"
+        DataGridView1.Columns(5).HeaderText = "Proveedor"
         'Proximamente Solo en Cines
     End Sub
 
