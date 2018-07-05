81b5710fe36a1f9f51224249d3e73f7e11ef4d41
LucasFerreira <luc45f3rr3ir4@gmail.com>
2018-07-05 14:16:58 -0300

    Mola

--- proyecto.inicio/My Project/Resources.Designer.vb
+++ proyecto.inicio/My Project/Resources.Designer.vb
@@ -233,6 +233,16 @@ Namespace My.Resources
         '''<summary>
         '''  Busca un recurso adaptado de tipo System.Drawing.Bitmap.
         '''</summary>
+        Friend ReadOnly Property salir1() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("salir1", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        '''</summary>
         Friend ReadOnly Property stock() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("stock", resourceCulture)
