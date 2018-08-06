0f66393e82d4201c4230551770cbae0ebde06305
francox17 <39157463+francox17@users.noreply.github.com>
2018-08-06 12:12:16 -0300

    corrigiendo errores

--- proyecto.inicio/Buscar proveedores.Designer.vb
+++ proyecto.inicio/Buscar proveedores.Designer.vb
@@ -23,14 +23,9 @@ Partial Class Buscar_proveedores
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.dgvfran = New System.Windows.Forms.DataGridView()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.txtbuscarprov = New System.Windows.Forms.TextBox()
         Me.ID = New System.Windows.Forms.Label()
-        Me.TextBox2 = New System.Windows.Forms.TextBox()
-        Me.TextBox3 = New System.Windows.Forms.TextBox()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.Label2 = New System.Windows.Forms.Label()
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.Button2 = New System.Windows.Forms.Button()
+        Me.btnvolver = New System.Windows.Forms.Button()
         CType(Me.dgvfran, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
@@ -42,85 +37,39 @@ Partial Class Buscar_proveedores
         Me.dgvfran.Size = New System.Drawing.Size(422, 287)
         Me.dgvfran.TabIndex = 0
         '
-        'TextBox1
+        'txtbuscarprov
         '
-        Me.TextBox1.Location = New System.Drawing.Point(58, 35)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
-        Me.TextBox1.TabIndex = 1
+        Me.txtbuscarprov.Location = New System.Drawing.Point(58, 35)
+        Me.txtbuscarprov.Name = "txtbuscarprov"
+        Me.txtbuscarprov.Size = New System.Drawing.Size(100, 20)
+        Me.txtbuscarprov.TabIndex = 1
         '
         'ID
         '
         Me.ID.AutoSize = True
-        Me.ID.Location = New System.Drawing.Point(12, 35)
+        Me.ID.Location = New System.Drawing.Point(12, 38)
         Me.ID.Name = "ID"
         Me.ID.Size = New System.Drawing.Size(40, 13)
         Me.ID.TabIndex = 2
         Me.ID.Text = "Buscar"
         '
-        'TextBox2
+        'btnvolver
         '
-        Me.TextBox2.Location = New System.Drawing.Point(58, 97)
-        Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
-        Me.TextBox2.TabIndex = 3
-        '
-        'TextBox3
-        '
-        Me.TextBox3.Location = New System.Drawing.Point(58, 119)
-        Me.TextBox3.Name = "TextBox3"
-        Me.TextBox3.Size = New System.Drawing.Size(100, 20)
-        Me.TextBox3.TabIndex = 4
-        '
-        'Label1
-        '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(12, 100)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(40, 13)
-        Me.Label1.TabIndex = 5
-        Me.Label1.Text = "Codigo"
-        '
-        'Label2
-        '
-        Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(12, 126)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(44, 13)
-        Me.Label2.TabIndex = 6
-        Me.Label2.Text = "Nombre"
-        '
-        'Button1
-        '
-        Me.Button1.Location = New System.Drawing.Point(164, 121)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
-        Me.Button1.TabIndex = 7
-        Me.Button1.Text = "Delete"
-        Me.Button1.UseVisualStyleBackColor = True
-        '
-        'Button2
-        '
-        Me.Button2.Location = New System.Drawing.Point(164, 95)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(75, 23)
-        Me.Button2.TabIndex = 8
-        Me.Button2.Text = "Buscar"
-        Me.Button2.UseVisualStyleBackColor = True
+        Me.btnvolver.Location = New System.Drawing.Point(83, 97)
+        Me.btnvolver.Name = "btnvolver"
+        Me.btnvolver.Size = New System.Drawing.Size(75, 23)
+        Me.btnvolver.TabIndex = 3
+        Me.btnvolver.Text = "Volver"
+        Me.btnvolver.UseVisualStyleBackColor = True
         '
         'Buscar_proveedores
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(675, 286)
-        Me.Controls.Add(Me.Button2)
-        Me.Controls.Add(Me.Button1)
-        Me.Controls.Add(Me.Label2)
-        Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.TextBox3)
-        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.btnvolver)
         Me.Controls.Add(Me.ID)
-        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.txtbuscarprov)
         Me.Controls.Add(Me.dgvfran)
         Me.Name = "Buscar_proveedores"
         Me.Text = "Buscar_proveedores"
@@ -130,12 +79,7 @@ Partial Class Buscar_proveedores
 
     End Sub
     Friend WithEvents dgvfran As System.Windows.Forms.DataGridView
-    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents txtbuscarprov As System.Windows.Forms.TextBox
     Friend WithEvents ID As System.Windows.Forms.Label
-    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
-    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
-    Friend WithEvents Label1 As System.Windows.Forms.Label
-    Friend WithEvents Label2 As System.Windows.Forms.Label
-    Friend WithEvents Button1 As System.Windows.Forms.Button
-    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents btnvolver As System.Windows.Forms.Button
 End Class
