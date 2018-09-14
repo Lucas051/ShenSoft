faf7c2466ebf0088ea8c1c682dbde5d36f7fe655
LucasFerreira <luc45f3rr3ir4@gmail.com>
2018-09-14 20:22:23 -0300

    Cambiando BD, agregando usuarios y modificando valores default en saldos

--- proyecto.sql
+++ proyecto.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1:3306
--- Tiempo de generación: 07-06-2018 a las 20:59:08
+-- Tiempo de generación: 14-09-2018 a las 23:18:48
 -- Versión del servidor: 5.7.21
 -- Versión de PHP: 5.6.35
 
@@ -33,11 +33,20 @@ CREATE TABLE IF NOT EXISTS `clientes` (
   `num_cliente` int(11) NOT NULL AUTO_INCREMENT,
   `CI` int(11) NOT NULL,
   `nombre_cliente` varchar(40) DEFAULT NULL,
-  `saldo_c` int(11) NOT NULL,
   `direccion_c` varchar(40) DEFAULT NULL,
   `tel_c` varchar(50) DEFAULT NULL,
+  `saldo_c` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`num_cliente`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `clientes`
+--
+
+INSERT INTO `clientes` (`num_cliente`, `CI`, `nombre_cliente`, `direccion_c`, `tel_c`, `saldo_c`) VALUES
+(1, 50789688, 'Fernando Torres', 'Luis Battle 201', '47332589', 0),
+(2, 50789688, 'Fernando Torres', 'Luis Battle 201', '47332589', 0),
+(3, 34567853, 'Jose Antonio', 'Artigas 2201', '47378541', 0);
 
 -- --------------------------------------------------------
 
@@ -132,9 +141,19 @@ CREATE TABLE IF NOT EXISTS `proveedores` (
   `nombre_prov` varchar(40) DEFAULT NULL,
   `tel_p` varchar(40) DEFAULT NULL,
   `direccion_p` varchar(40) DEFAULT NULL,
-  `saldo_p` int(11) NOT NULL,
+  `saldo_p` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`num_prov`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `proveedores`
+--
+
+INSERT INTO `proveedores` (`num_prov`, `nombre_prov`, `tel_p`, `direccion_p`, `saldo_p`) VALUES
+(1, 'Juan', '438567', 'Hola probando', 0),
+(2, 'PollosCalidad', '47334589', 'Barbieri 132', 0),
+(3, 'Carlitos', '099724567', 'Artigas 32', 0),
+(4, 'Frederik', '4738765', 'Yatay', 0);
 
 -- --------------------------------------------------------
 
@@ -152,16 +171,39 @@ CREATE TABLE IF NOT EXISTS `stock` (
   `num_prov` int(11) DEFAULT NULL,
   PRIMARY KEY (`cod_producto`),
   KEY `num_prov` (`num_prov`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `stock`
 --
 
 INSERT INTO `stock` (`cod_producto`, `descripcion`, `cantidad`, `precio_costo`, `precio_v_stock`, `num_prov`) VALUES
-(1, 'asdasd', NULL, 321, 444, NULL),
-(2, 'asdasd', '3', 233, 4444, NULL),
-(3, 'Hola anduvo', '23', 234, 3333, NULL);
+(14, 'Alitas', '50', 20, 100, 1),
+(15, 'Alitas Premium', '15', 50, 200, 2),
+(16, 'Milanesas de Pollo', '100', 20, 80, 2),
+(17, 'Milanesas de Carne', '100', 30, 100, 2),
+(18, 'Croquetas ', '80', 10, 30, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+CREATE TABLE IF NOT EXISTS `usuarios` (
+  `user` varchar(30) DEFAULT NULL,
+  `password` varchar(30) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`user`, `password`) VALUES
+('admin', 'admin'),
+('Dueño', 'lapolleria'),
+('Empleados', 'soyunempleado');
 
 --
 -- Restricciones para tablas volcadas
