5b1474cf6f23223e95bb097939507ddac33bf6d7
Ignacio <39157472+ignacioetchepare@users.noreply.github.com>
2018-10-11 07:54:42 -0300

    a empezar a trabajar

--- proyecto.inicio/My Project/Application.Designer.vb
+++ proyecto.inicio/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.proyecto.inicio.ConsultaFactura
+            Me.MainForm = Global.proyecto.inicio.Inicio
         End Sub
     End Class
 End Namespace
